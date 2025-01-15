58885d4df321160aebd62f62e765b12bf1d0c3fa
oderindesuliha <oderindesuliha@gmail.com>
2025-01-15 10:43:46 +0100

    PhaseGateOne

---
+++ PhaseGateOne/task_one.py
@@ -0,0 +1,9 @@
+firstNumber = 5
+secondNumber = 25
+sum = firstNumber + secondNumber
+result = int(input('enter the sum of the two numbers: '))
+
+if result == sum:
+	print('true')
+else:
+	print('false')
