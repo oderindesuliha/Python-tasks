1e8929a2174456ab004506ee73c5b64773ba70a0
oderindesuliha <oderindesuliha@gmail.com>
2025-02-21 22:10:20 -0800

    expense tracker implementation

---
+++ assignment/expense tracker/expensetrackerfunction.py
@@ -0,0 +1,19 @@
+class expenseapp:
+	def get_expensetracker():
+		choice()
+
+	def choice():
+		print("""Welcome to Semicolon Expense Tracker App
+		-----------------------------------------------------------
+	
+		options:
+		1. Add an expense
+		2. View all expenses
+		3. Calculate total expenses
+		4. Exit 
+		
+		""")
+
+		
+	
+
---
+++ assignment/expense tracker/test_expensetrackerfunction.py
@@ -0,0 +1,14 @@
+import unittest
+import expensetrackerfunction
+
+class Testexpensetracker(unittest.TestCase):
+	
+	def test_that_prime_number_function_exists(self):
+		expensetrackerfunction.get_expensetracker()
+
+	
+
+
+
+if __name__ =='__main__':
+	unittest.main()
