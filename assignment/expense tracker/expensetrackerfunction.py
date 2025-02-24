73bf84fe5b2ce9f8714754cd0babafee96b2268c
oderindesuliha <oderindesuliha@gmail.com>
2025-02-24 08:29:12 +0100

    Expense tracker

--- assignment/expense tracker/expensetrackerfunction.py
+++ assignment/expense tracker/expensetrackerfunction.py
@@ -1,27 +1,27 @@
 from datetime import datetime
 
-date_time = []
-description_list = []
-amount_list = []
+expenses = []
 
 def add_expense(date, description, amount):
-	date_time.append(date)
-	description_list.append(description)
-	amount_list.append(amount)
-	print("Expenses added!")
+	expenses.append({"date": date, "description": description, "amount": amount})  	
+	print("Expenses added!\n")
 	choice = int(input(option_list))
 	main_menu(choice)
 
 def view_expenses():
+	print("\nExpenses:")
 	count = 0
-	for count in range(len(description_list)):
-		print(f"{count + 1}. Date: {date_time[count]}, Description: {description_list[count]}, Amount: ₦{amount_list[count]:.2f}")	
+	for count in range(len(expenses)):
+		expense_list = expenses[count]
+		print(f"{count + 1}. Date: {expense_list['date']}, Description: {expense_list['description']}, Amount: ₦{expense_list['amount']:.2f}\n")
+			
 	choice = int(input(option_list))
 	main_menu(choice)
 
-def calculate_expenses():
-	total = sum(amount_list)  
-	print(f"Total Expenses: ₦{total:.2f}")  
+def calculate_total():
+	
+	total = sum(expense_list['amount']for expense_list in expenses)
+	print(f"\nTotal Expenses: ₦{total:.2f}\n")  
 	choice = int(input(option_list))
 	main_menu(choice)
 
@@ -32,31 +32,30 @@ def main_menu(choice):
 		choice = int(input("Please enter a valid option: "))
 
 	if choice == 1:
-		year=int(input("Please enter year: "))
+		year=int(input("\nPlease enter year: "))
 		while year < 1900 or year > datetime.now().year:
 			year = int(input("Invalid year..... enter a year between 1900 and 2025: "))
-		month=int(input("Please enter month: "))
+		month=int(input("\nPlease enter month: "))
 		while month < 1 or month > 12:
 			month = int(input("Invalid month..... enter a month between 1 and 12: "))
-		day=int(input("Please enter day: "))
+		day=int(input("\nPlease enter day: "))
 		while day < 1 or day > 31:
 			day = int(input("Invalid day...... enter a day between 1 and 31: "))
-			
-		#date = datetime(year, month, day).strftime('%Y-%m-%d')
-		date = str(year)+"-"+str(month)+"-"+str(day)
-		print(f"Enter the date(YYYY-MM-DD): {date}")
 		
-		description=input("Enter the description: ")
-		amount=float(input("Enter the amount: "))
+		date = f"{year}-{month:02d}-{day:02d}"
+		print(f"\nEnter the date(YYYY-MM-DD): {date}")
+		
+		description=input("\nEnter the description: ")
+		amount=float(input("\nEnter the amount: "))
 		while amount < 0:
 			amount = float(input("Invalid amount"))
-		print(add_expense(date, description, amount))
+		add_expense(date, description, amount)
 
 	if choice == 2:
 		view_expenses()
 
 	if choice == 3:
-		calculate_expenses()
+		calculate_total()
 
 	if choice == 4:
 		print("Exiting the app. Goodbye!")
@@ -70,17 +69,7 @@ option_list = ("1. Add an expense\n"
 		"2. View all expenses\n"
 		"3. Calculate total expenses\n"
 		"4. Exit\n"
-		"Enter your choice: ")
+		"\nEnter your choice: ")
 
 choice = int(input(option_list))
-main_menu(choice) 
-
-
-
-
-
-
-
-
-		
-		
+main_menu(choice)
--- assignment/expense tracker/test_expensetrackerfunction.py
+++ assignment/expense tracker/test_expensetrackerfunction.py
@@ -1,14 +1,15 @@
 import unittest
-import expensetrackerfunction
+from datetime import datetime
+from expensetrackerfunction import add_expense, view_expenses, calculate_total, expenses
+
 
 class Testexpensetracker(unittest.TestCase):
-	
-	def test_that_the_expense_tracker_function_exists(self):
-		expensetrackerfunction.get_expensetracker()
 
 	
 
 
+
+	
 
 if __name__ =='__main__':
 	unittest.main()
